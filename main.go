5e929b931e1d676d576dc04f8c66b96b28cb6e47
Fredric <fredric2010@outlook.com>
2019-06-16 17:05:22 +0800

    Second commit

--- main.go
+++ main.go
@@ -4,23 +4,7 @@ import (
 	_ "bapi/routers"
 
 	"github.com/astaxie/beego"
-	// "github.com/astaxie/beego/logs"
-	// "github.com/astaxie/beego/orm"
-	// _ "github.com/go-sql-driver/mysql"
 )
-
-func init() {
-	// orm.RegisterDriver("mysql", orm.DRMySQL)
-	// dbType := beego.AppConfig.String("dbType")
-	// sqlConn := beego.AppConfig.String("sqlconn")
-	// maxIdle, _ := beego.AppConfig.Int("maxIdle")
-	// maxConn, _ := beego.AppConfig.Int("maxConn")
-	// logs.Info("Connect to [%v] database with conn: [%v] \n", dbType, sqlConn)
-	// err := orm.RegisterDataBase("default", dbType, sqlConn, maxIdle, maxConn)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-}
 
 func main() {
 
